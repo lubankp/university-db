2fc901cce869eca84f5c080c2a06986f39ac0a27
Pawel Lubanski <pawel.lubanski.91@gmail.com>
2021-08-24 11:48:21 +0200

    student1 added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-
+#include <iostream>
+#include "student.cpp"
 
 int main{
+
+Student student1("Pawel", "Lubanski", "Tasmowa", 0, 91070207174, "m") 
+
+std::cout << student1.getName();
+std::cout << student1.getFamily_name();
+
 
 return 0
 }
--- student.cpp
+++ student.cpp
@@ -2,12 +2,12 @@
 class Student {
 
 private:
-    std::string name_;
-    std::string family_name_;
-    std::string address_;
-    size_t id_;
-    int pessel_;
-    std::string sex_;
+    std::string name_ = "";
+    std::string family_name_ = "";
+    std::string address_ = "";
+    size_t id_ = 0;
+    int pessel_ = 0;
+    std::string sex_ = "";
 
 public:
 
@@ -18,4 +18,7 @@ public:
     int getPesel(){ return pessel_; }
     std::string getSex() {return sex_; }
 
+    Student(std::string name, std::string family_name, std::string address, size_t id, int pessel, std::string sex)
+        : name_(name), family_name_(family_name), address_(address), id_(id), pessel_(pessel), sex_(sex)
+    {}
 }
